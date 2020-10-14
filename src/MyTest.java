b33ae5f204963e4aeac7c0edc80f9b5f15d620ad
rishika7-hub <srishika970@gmail.com>
2020-10-14 09:41:22 +0530

    Second Commit

--- src/MyTest.java
+++ src/MyTest.java
@@ -4,7 +4,13 @@ public class MyTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is my test program");
+		
+		// UC1- Testing
+		
+		System.out.println("Testing Started");
+			
+		}
 
 	}
 
-}
+
